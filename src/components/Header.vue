78153f63c4c1345792cd1533c6e3c6112f51e09f
shelby <>
2024-03-27 17:21:35 +0800

    remove vue stuff and add my own stuff

---
+++ src/components/Header.vue
@@ -0,0 +1,28 @@
+<template>
+  <header>
+    <div class="header">
+      <text>Vue task manager</text>
+      <button @click="openModal" class="createTask">Create Task</button>
+    </div>
+  </header>
+</template>
+
+<script setup lang="ts">
+import { modalStore } from '@/stores/modalStore'
+const modalData = modalStore()
+const { openModal } = modalData // use the openModal function in modalStore
+</script>
+
+<style scoped>
+.header {
+  display: flex;
+  place-items: center;
+  justify-content: space-between;
+  /* padding-right: calc(var(--section-gap) / 2); */
+}
+.createTask {
+  flex-direction: row;
+  justify-content: space-between;
+}
+</style>
+@/stores/modalStore
--- src/stores/modalStore.ts
+++ src/stores/modalStore.ts
@@ -1,11 +1,19 @@
 import { ref, computed } from 'vue'
 import { defineStore } from 'pinia'
 
-export const useStore = defineStore('task', {
+export const modalStore = defineStore('task', {
   state: () => ({
-    allTask: {}
+    allTask: {},
+    showModal: false
   }),
-  actions: [],
+  actions: {
+    openModal() {
+      this.showModal = true
+    },
+    closeModal() {
+      this.showModal = false
+    }
+  },
   /**  https://www.npmjs.com/package/pinia-plugin-persistedstate
    *  Persists this store's state in localStorage to reuse across sessions
    *  Note that this is not efficient for large to extremely large state trees,
